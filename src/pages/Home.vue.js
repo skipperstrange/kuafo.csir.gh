7943fbd3ebaead6b157f90381d71415aa970157e
Skipper Strange <skipperstrange@gmail.com>
2021-09-01 00:51:33 +0000

    basic framework complete

--- src/pages/Home.vue.js
+++ src/pages/Home.vue.js
@@ -6,10 +6,11 @@ define(["Vue", "JQuery"], function(Vue) {
             }
         },
         methods: {
-            SayHi(){
-                alert(this.intro);
-                
-            }
+                SayHi(){
+                    alert(this.intro);
+                    
+                }
+            
         },
         mounted(){
             $(()=>{
@@ -20,14 +21,12 @@ define(["Vue", "JQuery"], function(Vue) {
             });
         },
         template: `
-        
             <div class="uk-container">
-            
                 <div class="uk-margin uk-card uk-card-default uk-card-body uk-margin-top">
                     <h1 class="uk-heading-small">im a Home</h1>
                     <p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <router-link class="uk-button uk-button-default" :to="{name: 'Article'}">Go to Article</router-link>
-                    Or <button @click="this.SayHi()">Say hi</button>
+                    Or <button @click="SayHi()">Say hi</button>
                 </div>
             </div>
         `
